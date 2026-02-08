406ede7ea628bde9d1373932b03ed2f57f657d5e
Aarthitechnology <aarthi908785@gmail.com>
2026-02-08 15:47:56 +0530

    Working of add subject button and dynamic output of subject rows

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("JavaScript is Connected");
+let addbutton = document.getElementById("add");
+let subjectcountinput = document.getElementById("subjectcount");
+let subjectcontainer = document.getElementById("subject-row");
+addbutton.addEventListener("click",function() {
+    let count = subjectcountinput.value;
+    subjectcontainer.innerHTML="";
+    for(let i=1;i<=count;i++)
+    {
+        subjectcontainer.innerHTML += `<h4>Subject ${i}</h4>
+        <label>Subject Name:</label>
+            <input type="text" placeholder="Enter Subject Name"><br><br>
+
+            <label>Credit Points:</label>
+            <input type="number" placeholder="Enter Credit Points"><br><br>
+
+            <label>Grade:</label>
+            <select>
+                <option value="" disabled selected>Select Grade</option>
+                <option value="O">O</option>
+                <option value="A+">A+</option>
+                <option value="A">A</option>
+                <option value="B+">B+</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="RA">RA</option>
+            </select>
+            <br><br>
+        `;
+    }
+});
